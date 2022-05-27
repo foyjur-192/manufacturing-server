d8f424397bb16cf9db914cd58e1c6fa896ccc662
foyjur-192 <97134368+foyjur-192@users.noreply.github.com>
2022-05-27 23:50:43 +0600

    newline

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
+//for jwt token function
 
 // function verifyJWT (req, res, next){
 //   const authHeader = req.headers.authorization;
